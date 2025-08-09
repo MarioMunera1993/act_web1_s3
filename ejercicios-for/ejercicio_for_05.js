1a4f45046cac8fcaacc4809615ba1aef1e67dfc1
Mario Munera <samuelmariomunera@gmail.com>
2025-08-09 16:53:06 -0500

    Datos del json

--- ejercicios-for/ejercicio_for_05.js
+++
@@ -1,3 +0,0 @@
-// Ejercicio For 5: Invertir Cadenas de Texto
-// Crea un programa que use un ciclo for para invertir una cadena de texto carácter por carácter. 
-// Muestra tanto la cadena original como la invertida.
--- ejercicios-for/ejercicio_for_06.js
+++
@@ -1,3 +0,0 @@
-// Ejercicio For 6: Calculadora de Potencias
-// Implementa una función que use un ciclo for para calcular la potencia de un número sin usar Math.pow(). 
-// Debe mostrar el proceso paso a paso.
