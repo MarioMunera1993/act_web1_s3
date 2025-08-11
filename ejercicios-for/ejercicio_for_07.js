e679ca4a418097f3bc544b28a87dad9e8bd82313
Mario Munera <samuelmariomunera@gmail.com>
2025-08-11 16:18:03 -0500

    Fin todos los ejercicios

--- ejercicios-for/ejercicio_for_07.js
+++ ejercicios-for/ejercicio_for_07.js
@@ -1,7 +1,6 @@
 let matriz = [];
 let i, j;
 
-// Crear la matriz 5x5 con suma de índices
 for (i = 0; i < 5; i++) {
   matriz[i] = [];
   for (j = 0; j < 5; j++) {
@@ -9,7 +8,6 @@ for (i = 0; i < 5; i++) {
   }
 }
 
-// Mostrar la matriz formateada
 let salida = "";
 for (i = 0; i < 5; i++) {
   for (j = 0; j < 5; j++) {
